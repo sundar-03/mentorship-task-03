cbba29cbaf38c8bd5ceca19300072dabb721b2c6
sundar-03 <56792860+sundar-03@users.noreply.github.com>
2020-06-14 21:28:09 +0530

    Create leftcol.php

---
+++ leftcol.php
@@ -0,0 +1,16 @@
+<script type="text/javascript">
+	function good() {
+		<?php
+             header("location:newinvitation2.php");
+		?>
+	}
+</script>
+
+<div id="left-col-container">
+	<div onclick="good()" style="cursor: pointer;" class="grey-back">
+		<p align="center">
+			Create New Invitation
+		</p>
+	</div>
+		
+		</div>

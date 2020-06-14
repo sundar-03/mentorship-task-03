ac413b01333418471058dceb4a558596a97ca76a
sundar-03 <56792860+sundar-03@users.noreply.github.com>
2020-06-14 21:24:08 +0530

    Create style.php

---
+++ style.php
@@ -0,0 +1,114 @@
+html,body
+	{
+		height: 100%;
+		overflow: hidden;
+		padding: 0px;
+		margin: 0px;
+	}
+a,p
+	{
+		font-size: 12px;
+		font-family:helvetica;
+	}
+#container
+{
+	box-shadow: 2px 2px 2px #000000;
+	width: 1200px;
+	height: 90%;
+	margin: 2% auto;
+	border-radius: 1%;
+	overflow: hidden;
+}
+#menu
+{
+	background: #233070;
+	color: white;
+	padding: 1%;
+	font-size: 30px;
+}
+#left-col, #right-col
+{
+	position: relative;
+	float: left;
+   height: 90%;
+
+}
+#left-col
+{
+	width: 30%;
+}
+#right-col
+{
+	width: 69%;
+	border: 1px solid #efefef;
+}
+#left-col-container ,#right-col-container
+{
+	width: 100%;
+	height: 100%;
+	margin: 0px auto;
+	height: 100%;
+	overflow: auto;
+}
+.greyback
+{
+	border: 1px solid black;
+	padding: 5px;
+	background: #efefef;
+	margin: 0px auto;
+	margin-top: 2px;
+	overflow:auto;
+}
+ .white-invitation,.grey-invitation
+{
+border: 1px solid black;
+width: 96%;
+padding: 5px;
+margin: 0px auto;
+margin-top: 2px;
+overflow: auto;
+}
+.white-invitation
+{
+	background:white;
+}
+.grey-invitation
+{
+  background: #efefef;
+}
+#new-invitation
+{
+		display: none;
+		box-shadow: 2px 10px 30px #000000;
+		width: 500px;
+		position: fixed;
+		top: 10%;
+		background: white;
+		z-index: 2;
+		left: 50%;
+		transform: translate(-50%,0);
+		border-radius: 5px;
+		overflow: auto;
+}
+.invit-header,.invit-footer
+{
+		background: #233070;
+		margin: 0px;
+		color: white;
+		padding: 5px;
+}
+.invit-header
+{
+font-size: 20px;
+text-align: center;
+}
+.invit-body
+{
+padding: 5px;
+
+}
+.invit-input
+{
+	width: 96%;
+
+}

79d5fe11d660764e46ae1c2d4ba0a020a9103003
sundar-03 <56792860+sundar-03@users.noreply.github.com>
2020-06-16 21:46:23 +0530

    Update leftcol.php

--- leftcol.php
+++ leftcol.php
@@ -1,16 +1,31 @@
-<script type="text/javascript">
-	function good() {
-		<?php
-             header("location:newinvitation2.php");
-		?>
-	}
-</script>
-
 <div id="left-col-container">
-	<div onclick="good()" style="cursor: pointer;" class="grey-back">
+	<a href="newinvitation2.php">
+	<div  style="cursor: pointer;" class="whiteback">
 		<p align="center">
 			Create New Invitation
 		</p>
 	</div>
-		
+</a>
+		<?php
+			require_once ("connection.php");
+			 $q="SELECT * FROM feedback WHERE sender_name='".$_SESSION['username']."' ORDER BY date_time DESC ";
+			$r=mysqli_query($con,$q);
+			while ($row = mysqli_fetch_assoc($r))
+			{
+				$sender_name=$row['sender_name'];
+				$receiver_name = $row['receiver_name'];
+				$status = $row['status'];
+				?>
+				<div  style="cursor: pointer;" class="greyback">
+						<p align="center">
+						 <?php
+						    echo $receiver_name." has ".$status."ed your invitation";
+						 ?>
+						</p>
+					</div>
+
+					<?php
+			}
+			?>
+         
 		</div>
